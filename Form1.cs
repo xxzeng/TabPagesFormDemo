f51420b436b841b386484008c4755a94985963b6
xxzeng <k85097ok@gmail.com>
2015-02-09 14:59:04 +0800

    TabPagesFormDemo

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TabPagesFormDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.TopLevel = false;
+            f2.Visible = true;
+            tabControl1.Dock = DockStyle.Fill;
+            tabPage1.Controls.Add(f2);
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TabPagesFormDemo
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_query = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_clear.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_clear.Location = new System.Drawing.Point(489, 63);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(82, 90);
+            this.btn_clear.TabIndex = 45;
+            this.btn_clear.Text = "離開";
+            this.btn_clear.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_export
+            // 
+            this.btn_export.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_export.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_export.Location = new System.Drawing.Point(401, 63);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(82, 90);
+            this.btn_export.TabIndex = 44;
+            this.btn_export.Text = "轉EXCEL";
+            this.btn_export.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_export.UseVisualStyleBackColor = true;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_save.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_save.Location = new System.Drawing.Point(313, 63);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(82, 90);
+            this.btn_save.TabIndex = 43;
+            this.btn_save.Text = "儲存";
+            this.btn_save.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // btn_query
+            // 
+            this.btn_query.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_query.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_query.Location = new System.Drawing.Point(225, 63);
+            this.btn_query.Name = "btn_query";
+            this.btn_query.Size = new System.Drawing.Size(82, 90);
+            this.btn_query.TabIndex = 42;
+            this.btn_query.Text = "查詢";
+            this.btn_query.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_query.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 22);
+            this.textBox1.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 61);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Here is Form2";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 318);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_query);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_export;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_query;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
